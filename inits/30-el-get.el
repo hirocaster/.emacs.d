8974476caf6eec9d2e1685b9c79a59aa820611d1
hirocaster <hohtsuka@gmail.com>
2014-01-25 10:18:03 +0900

    Setup el-get

---
+++ inits/30-el-get.el
@@ -0,0 +1,12 @@
+(setq el-get-dir "~/.emacs.d/vendor/el-get")
+(add-to-list 'load-path "~/.emacs.d/vendor/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+    (with-current-buffer
+              (url-retrieve-synchronously
+                      "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+          (goto-char (point-max))
+              (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/vendor/el-get/recipes")
+(el-get 'sync)
---
+++ vendor/el-get/.status.el
@@ -0,0 +1,19 @@
+((cl-lib status "installed" recipe
+         (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (el-get status "installed" recipe
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (git-modes status "installed" recipe
+            (:name git-modes :description "GNU Emacs modes for various Git-related files" :type github :pkgname "magit/git-modes"))
+ (magit status "installed" recipe
+        (:name magit :website "https://github.com/magit/magit#readme" :description "It's Magit! An Emacs mode for Git." :type github :pkgname "magit/magit" :depends
+               (cl-lib git-modes)
+               :info "." :build
+               (if
+                   (version<= "24.3" emacs-version)
+                   `(("make" ,(format "EMACS=%s" el-get-emacs)
+                      "all"))
+                 `(("make" ,(format "EMACS=%s" el-get-emacs)
+                    "docs")))
+               :build/berkeley-unix
+               (("touch" "`find . -name Makefile`")
+                ("gmake")))))

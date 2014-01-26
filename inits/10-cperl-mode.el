7bf95290d675d40f79b5f43a73be61979eaad3fc
hirocaster <hohtsuka@gmail.com>
2014-01-26 22:35:08 +0900

    Setup cperl-mode

---
+++ inits/10-cperl-mode.el
@@ -0,0 +1,5 @@
+(defalias 'perl-mode 'cperl-mode)
+(setq auto-mode-alist (cons '("\\.t$" . cperl-mode) auto-mode-alist))
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (cperl-set-style "PerlStyle")))

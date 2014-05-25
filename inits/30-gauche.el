0a4f650ee146601bb2ee1581e09733d57e5e3a7d
hirocaster <hohtsuka@gmail.com>
2014-05-25 21:18:04 +0900

    Setup gauche

---
+++ inits/30-gauche.el
@@ -0,0 +1,16 @@
+(setq process-coding-system-alist
+      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+
+(setq scheme-program-name "/usr/bin/env gosh -i")
+
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
+(defun scheme-other-window ()
+  "Run scheme on other window"
+  (interactive)
+  (switch-to-buffer-other-window
+   (get-buffer-create "*scheme*"))
+  (run-scheme scheme-program-name))
+(define-key global-map
+  "\C-cS" 'scheme-other-window)

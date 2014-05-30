66303811bc9fe92079f1759c9153e22f4470eaf5
hirocaster <hohtsuka@gmail.com>
2014-05-30 12:24:18 +0900

    Speed up helm

--- inits/40-helm.el
+++ inits/40-helm.el
@@ -1,5 +1,8 @@
 (el-get 'sync 'helm)
 
+(setq helm-idle-delay 0.1)
+(setq helm-input-idle-delay 0.1)
+
 (helm-mode 1)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)

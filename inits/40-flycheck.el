f3d64f55d8bd57752b81e05e30108a395755eb6f
hirocaster <hohtsuka@gmail.com>
2014-06-07 19:46:10 +0900

    Improve flycheck-mode display settings

--- inits/40-flycheck.el
+++ inits/40-flycheck.el
@@ -1,2 +1,2 @@
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-;; (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+(setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
+(add-hook 'after-init-hook #'global-flycheck-mode)

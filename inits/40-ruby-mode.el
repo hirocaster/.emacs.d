ea9baedb7ab1eaf0701aa988ebad3c4cdd69cd92
hirocaster <hohtsuka@gmail.com>
2014-06-09 14:50:21 +0900

    Set major mode *.rake files

--- inits/40-ruby-mode.el
+++ inits/40-ruby-mode.el
@@ -7,6 +7,7 @@
           (define-key motion-mode-map (kbd "C-c C-p") 'motion-convert-code-region))
 
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Capfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))

3f5d1de0e70258df20f6f7852a4f44df4c565f98
hirocaster <hohtsuka@gmail.com>
2014-06-10 11:47:51 +0900

    Setup recenf

---
+++ inits/10-recentf-mode.el
@@ -0,0 +1,7 @@
+(when (require 'recentf nil t)
+  (setq recentf-max-saved-items 2000)
+  (setq recentf-exclude '(".recentf"))
+  (setq recentf-auto-cleanup 10)
+  (setq recentf-auto-save-timer
+        (run-with-idle-timer 30 t 'recentf-save-list))
+  (recentf-mode 1))

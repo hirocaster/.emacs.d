94b8189b385f5523befef46dd8b30b3d641559b8
hirocaster <hohtsuka@gmail.com>
2014-06-23 12:20:49 +0900

    Change key-bind cua-mode

--- inits/10-cua.el
+++ inits/10-cua.el
@@ -1,3 +1,3 @@
 (cua-mode t)
 (setq cua-enable-cua-keys nil) ;; 変なキーバインド禁止
-(define-key global-map (kbd "C-RET") 'cua-set-rectangle-mark) ;; 矩形選択
+(define-key global-map (kbd "C-x SPC") 'cua-set-rectangle-mark) ;; 矩形選択

9d5e8e63ab2cfe95b42455ac35935fd39a694ca7
hirocaster <hohtsuka@gmail.com>
2014-08-05 21:46:48 +0900

    Add helm-swoop

---
+++ inits/41-helm-swoop.el
@@ -0,0 +1,7 @@
+;; isearch実行中にhelm-swoopに移行
+(define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+;; helm-swoop実行中にhelm-multi-swoop-allに移行
+(define-key helm-swoop-map (kbd "M-i") 'helm-multi-swoop-all-from-helm-swoop)
+
+;; Save buffer when helm-multi-swoop-edit complete
+(setq helm-multi-swoop-edit-save t)
--- inits/90-bind-key.el
+++ inits/90-bind-key.el
@@ -29,6 +29,12 @@
 ;; helm-c-yas-complete
 (global-set-key (kbd "C-c y") 'helm-c-yas-complete)
 
+;; helm-swoop
+(global-set-key (kbd "M-i") 'helm-swoop)
+(global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
+(global-set-key (kbd "C-c M-i") 'helm-multi-swoop)
+(global-set-key (kbd "C-x M-i") 'helm-multi-swoop-all)
+
 ;; expand-region
 (global-set-key (kbd "C-=") 'er/expand-region)
 

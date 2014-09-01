fb3c6d6aa61e88c98815dc6fe6a54f24bf14afe8
hirocaster <hohtsuka@gmail.com>
2014-09-01 12:14:07 +0900

    Delete coustom color

--- init.el
+++ init.el
@@ -2,27 +2,3 @@
 (cask-initialize)
 
 (init-loader-load "~/.emacs.d/inits")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(eshell-ask-to-save-history (quote always))
- '(eshell-history-size 1000)
- '(eshell-ls-dired-initial-args (quote ("-h")))
- '(eshell-ls-exclude-regexp "~\\'")
- '(eshell-ls-initial-args "-h")
- '(eshell-ls-use-in-dired t nil (em-ls))
- '(eshell-modules-list (quote (eshell-alias eshell-basic eshell-cmpl eshell-dirs eshell-glob eshell-hist eshell-ls eshell-pred eshell-prompt eshell-rebind eshell-script eshell-smart eshell-term eshell-unix eshell-xtra)))
- '(eshell-prefer-to-shell t nil (eshell))
- '(eshell-stringify-t nil)
- '(eshell-term-name "ansi")
- '(eshell-visual-commands (quote ("vi" "top" "screen" "less" "lynx" "ssh" "rlogin" "telnet")))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(error ((t (:background "red" :foreground "Pink" :weight bold))))
- '(warning ((t (:background "yellow" :foreground "red" :weight bold)))))

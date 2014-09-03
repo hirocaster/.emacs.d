78c026b1ab0fd5da8624aeb3d1524642ec5020e1
hirocaster <hohtsuka@gmail.com>
2014-09-03 20:05:43 +0900

    Change mozc path

--- inits/10-mozc.el
+++ inits/10-mozc.el
@@ -1,4 +1,2 @@
-;; (require 'mozc)
-(load "~/src/mozc/src/unix/emacs/mozc.el")
 (setq default-input-method "japanese-mozc")
-(setq mozc-helper-program-name "~/src/mozc/src/out_mac/Release/mozc_emacs_helper")
+(setq mozc-helper-program-name "mozc_emacs_helper")

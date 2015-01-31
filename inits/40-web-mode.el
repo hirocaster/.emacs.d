70601e36e8acc3716e6c87b6ec59d509cc1477f0
hirocaster <hohtsuka@gmail.com>
2015-01-31 11:00:05 +0900

    Reconfig indent

--- inits/40-web-mode.el
+++ inits/40-web-mode.el
@@ -6,8 +6,8 @@
 
           (defun web-mode-hook ()
             "Hooks for Web mode."
-            (setq web-mode-html-offset   2)
-            (setq web-mode-css-offset    2))
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2))
 
           (add-hook 'web-mode-hook 'web-mode-hook))
 

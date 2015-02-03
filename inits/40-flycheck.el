576a823b860769371c33200064ef7f647aafa9ab
hirocaster <hohtsuka@gmail.com>
2015-02-03 16:02:19 +0900

    Set ruby-rubocop for flycheck ruby lint checker

--- inits/40-flycheck.el
+++ inits/40-flycheck.el
@@ -7,3 +7,8 @@
 (eval-after-load 'flycheck
     '(custom-set-variables
          '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (setq flycheck-checker 'ruby-rubocop)
+             (flycheck-mode 1)))

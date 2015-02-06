ce14b2eb1242a799376c8814c64e8a4d8d6a8527
hirocaster <hohtsuka@gmail.com>
2015-02-06 23:18:29 +0900

    Auto mejar-mode set coffeescript

--- inits/40-coffee-mode.el
+++ inits/40-coffee-mode.el
@@ -1,3 +1,5 @@
 (lazyload (coffee-mode) "coffee-mode"
           (set (make-local-variable 'tab-width) 2)
           (setq coffee-tab-width 2))
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))

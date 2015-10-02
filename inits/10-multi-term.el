b66e542015c28939133f9b32d0e17c4e36f8fc48
hirocaster <hohtsuka@gmail.com>
2015-10-02 13:56:31 +0900

    Fix init boot

--- inits/10-multi-term.el
+++ inits/10-multi-term.el
@@ -1,4 +1,4 @@
 (add-hook 'term-mode-hook '(lambda ()
                              (setq show-trailing-whitespace nil)
                              (add-to-list 'term-unbind-key-list '"C-t")
-                             (define-key term-raw-map (kbd "\C-z") 'term-send-raw))))
+                             (define-key term-raw-map (kbd "\C-z") 'term-send-raw)))

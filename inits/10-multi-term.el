ba52fe317e0c1363bad493b8d59c8131a8bf7ce8
hirocaster <hohtsuka@gmail.com>
2015-10-03 17:11:55 +0900

    Add unbind keys

--- inits/10-multi-term.el
+++ inits/10-multi-term.el
@@ -1,4 +1,6 @@
 (add-hook 'term-mode-hook '(lambda ()
                              (setq show-trailing-whitespace nil)
                              (add-to-list 'term-unbind-key-list '"C-t")
+                             (add-to-list 'term-unbind-key-list '"C-a")
+                             (add-to-list 'term-unbind-key-list '"C-e")
                              (define-key term-raw-map (kbd "\C-z") 'term-send-raw)))

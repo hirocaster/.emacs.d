92c2a3e4dec3212758c42ee63908115bac692e6a
hirocaster <hohtsuka@gmail.com>
2015-11-18 15:27:32 +0900

    Set M-. multiterm

--- inits/10-multi-term.el
+++ inits/10-multi-term.el
@@ -1,6 +1,11 @@
+(setq term-bind-key-alist
+    (list
+    (cons "M-."  'term-send-raw-meta)
+    ))
+
 (add-hook 'term-mode-hook '(lambda ()
                              (setq show-trailing-whitespace nil)
                              (add-to-list 'term-unbind-key-list '"C-t")
                              (add-to-list 'term-unbind-key-list '"C-a")
                              (add-to-list 'term-unbind-key-list '"C-e")
-                             (define-key term-raw-map (kbd "\C-z") 'term-send-raw)))
+                             (define-key term-raw-map (kbd "C-z") 'term-send-raw)))

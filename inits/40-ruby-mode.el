24ac1dfa313e90b0731d52274271c3c8eb52853c
hirocaster <hohtsuka@gmail.com>
2015-12-18 22:35:56 +0900

    Move config

--- inits/40-ruby-mode.el
+++ inits/40-ruby-mode.el
@@ -1,12 +1,13 @@
 (lazyload (ruby-mode) "ruby-mode"
           (setq ruby-insert-encoding-magic-comment nil)
-          (require 'motion-mode)
-          (add-hook 'ruby-mode-hook 'motion-recognize-project)
 
           (setq enh-ruby-add-encoding-comment-on-save nil)
           (add-hook 'ruby-mode-hook 'enh-ruby-mode)
 
           (add-to-list 'ac-sources 'ac-source-dictionary)
+
+          (require 'motion-mode)
+          (add-hook 'ruby-mode-hook 'motion-recognize-project)
           (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
           (define-key motion-mode-map (kbd "C-c C-d") 'motion-dash-at-point)
           (define-key motion-mode-map (kbd "C-c C-p") 'motion-convert-code-region))

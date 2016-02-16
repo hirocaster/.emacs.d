7550d7d094b2746f5fdea9a011792f9b7b339f5b
hirocaster <hohtsuka@gmail.com>
2016-02-16 23:10:13 +0900

    Add azik setting

--- inits/40-ddskk.el
+++ inits/40-ddskk.el
@@ -3,6 +3,7 @@
 (setq skk-share-private-jisyo t)
 (setq skk-jisyo-code 'utf-8-unix)
 (setq skk-use-azik t)
+(setq skk-azik-keyboard-type 'en)
 
 (setq skk-show-candidates-always-pop-to-buffer t) ; 変換候補の表示位置
 (setq skk-henkan-show-candidates-rows 2)          ; 候補表示件数

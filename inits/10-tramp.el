988a58aae681ef96968ee4c74b6d8ee44b8b476a
hirocaster <hohtsuka@gmail.com>
2016-11-23 00:25:18 +0900

    Add sudo

--- inits/10-tramp.el
+++ inits/10-tramp.el
@@ -1,3 +1,11 @@
 (require 'tramp)
 
 (setq tramp-password-prompt-regexp "^.*\\([pP]assword\\|[pP]assphrase\\|Verification code\\).*:? *")
+
+(defun sudo ()
+  "Use TRAMP to `sudo' the current buffer"
+  (interactive)
+  (when buffer-file-name
+    (find-alternate-file
+     (concat "/sudo:root@localhost:"
+             buffer-file-name))))

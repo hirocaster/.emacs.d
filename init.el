767ae9ef099570d3496bce38b4070f1668367ec8
hirocaster <hohtsuka@gmail.com>
2017-02-04 00:08:42 +0900

    Change cask to el-get

--- init.el
+++ init.el
@@ -1,14 +1,11 @@
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
-(setq gc-cons-threshold (* 128 1024 1024))
-
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-
+(package-install 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
-(init-loader-load "~/.emacs.d/secrets")

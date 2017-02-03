3b4cf005c94406fd41da68bfc5cdb5b38c78bcd5
hirocaster <hohtsuka@gmail.com>
2017-02-04 00:20:12 +0900

    Fix batch mode exec

--- inits/00-display.el
+++ inits/00-display.el
@@ -28,20 +28,7 @@
 
 (load-theme 'manoj-dark t)
 
-;;; 1行80文字を超えるとハイライト
-(add-hook 'ruby-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-(add-hook 'php-mode-hook
-  (lambda ()
-    (font-lock-add-keywords nil
-      '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t)))))
-
 ;; Mac Font
 (set-face-attribute 'default nil
                     :family "Ricty"
                     :height 180)
-(set-fontset-font
- nil 'japanese-jisx0208
- (font-spec :family "Ricty"))

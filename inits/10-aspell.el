bc688ae79d7af4b29f570f41bc240dbd68649450
hirocaster <hohtsuka@gmail.com>
2017-04-14 14:15:33 +0900

    Setup english dictionary for aspell

--- inits/10-aspell.el
+++ inits/10-aspell.el
@@ -2,5 +2,8 @@
 (setq flyspell-issue-welcome-flag nil)
 (setq ispell-extra-args '("--sug-mode=ultra"))
 
+(setq ispell-grep-command "grep")
+(setq ispell-alternate-dictionary (expand-file-name "~/.emacs.d/vendor/dictionary/en.dic"))
+
 (eval-after-load "ispell"
   '(add-to-list 'ispell-skip-region-alist '("[^\000-\377]+")))

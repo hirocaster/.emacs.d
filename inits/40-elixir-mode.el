25e85ceb962e530bdb0027649c2a49f13470b787
hirocaster <hohtsuka@gmail.com>
2017-12-02 00:45:34 +0900

    Add flycheck for Elixir

--- inits/40-elixir-mode.el
+++ inits/40-elixir-mode.el
@@ -5,3 +5,9 @@
 (add-to-list 'elixir-mode-hook 'ac-alchemist-setup)
 (add-to-list 'elixir-mode-hook 'alchemist-mode t)
 (add-to-list 'elixir-mode-hook 'auto-highlight-symbol-mode)
+
+(setq flycheck-elixir-credo-strict t)
+
+(eval-after-load 'flycheck
+  '(flycheck-credo-setup))
+(add-hook 'elixir-mode-hook 'flycheck-mode)

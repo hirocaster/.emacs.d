b8d060f509ea1e2de8748e628c3b49b4a542336a
hirocaster <hohtsuka@gmail.com>
2017-12-02 00:45:58 +0900

    Set indent for rjsx-mode

---
+++ inits/40-rjsx-mode.el
@@ -0,0 +1,5 @@
+(add-hook 'rjsx-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (setq js-indent-level 2)
+            (setq js2-strict-missing-semi-warning nil)))

8d7861496bac4fcc95541b6b9d965db2569680d1
hirocaster <hohtsuka@gmail.com>
2018-03-21 11:28:11 +0900

    Add config for org-mode

--- inits/50-org.el
+++ inits/50-org.el
@@ -2,6 +2,13 @@
 
 (add-to-list 'ac-modes 'org-mode)
 
+(setq org-startup-indented t)
+(setq org-indent-mode-turns-on-hiding-stars nil)
+(setq org-indent-indentation-per-level 4)
+(setq org-startup-folded 'content)
+
+(setq org-startup-with-inline-images t)
+
 ;; Set to the location of your Org files on your local system
 (setq org-directory "~/Dropbox/org")
 

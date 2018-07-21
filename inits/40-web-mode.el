73dd72f2d9f79662807b2e4553733ae19e28aebb
hirocaster <hohtsuka@gmail.com>
2018-07-22 02:32:03 +0900

    Add jsx for web-mode

--- inits/40-web-mode.el
+++ inits/40-web-mode.el
@@ -17,3 +17,17 @@
 (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx.js\\'"     . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)

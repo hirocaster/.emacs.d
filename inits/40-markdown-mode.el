bb48f35d965192d61b8697c20a0e6db16faf2941
hirocaster <hohtsuka@gmail.com>
2018-12-02 04:26:06 +0900

    Fix markdown-live-preview-mode

--- inits/40-markdown-mode.el
+++ inits/40-markdown-mode.el
@@ -1,3 +1,6 @@
+(setq markdown-command (string-trim (shell-command-to-string "type multimarkdown | awk {'print $3'}")))
+(setq markdown-preview-stylesheets (list (locate-user-emacs-file "vendor/css/github.css")))
+
 (lazyload (markdown-mode) "markdown-mode")
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
@@ -8,16 +11,4 @@
     (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
     (hide-sublevels 3)))
 
-(when (eq system-type 'darwin)
-  (shell-command-to-string "type multimarkdown | awk {'print $3'}")
-  (defun markdown-preview-file ()
-    "run Marked on the current file and revert the buffer"
-    (interactive)
-    (shell-command
-     (format "open -a /Applications/Marked.app %s"
-             (shell-quote-argument (buffer-file-name)))))
-  (global-set-key "\C-cp" 'markdown-preview-file))
-
 (add-to-list 'ac-modes 'markdown-mode)
-
-(setq markdown-preview-stylesheets (list (locate-user-emacs-file "vendor/css/github.css")))

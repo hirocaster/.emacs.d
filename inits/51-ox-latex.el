3de6a3ad6b9ff19408e4ac564a2fc7c7616f1303
hirocaster <hohtsuka@gmail.com>
2019-07-23 01:18:19 +0900

    Add setup export latex to pdf from org-mode

---
+++ inits/51-ox-latex.el
@@ -0,0 +1,53 @@
+;; Exporting
+(setq org-latex-pdf-process
+      '("ptex2pdf -l -ot '-synctex=1 -shell-escape' %f"
+        "ptex2pdf -l -ot '-synctex=1 -shell-escape' %f"
+        "ptex2pdf -l -ot '-synctex=1 -shell-escape' %f"))
+
+(add-to-list 'org-latex-classes
+             '("jsarticle" "\\documentclass{jsarticle}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-latex-classes
+             '("jsreport" "\\documentclass[report]{jsbook}"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+(add-to-list 'org-latex-classes
+             '("jsbook" "\\documentclass{jsbook}"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+(add-to-list 'org-latex-classes
+             '("jsarticle2" "\\documentclass[twocolumn,10pt]{jsarticle}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+(setq org-latex-default-packages-alist
+      '(("AUTO" "inputenc"  t)
+        ("T1"   "fontenc"   t)
+        (""     "fixltx2e"  nil)
+        ("dvipdfmx"     "graphicx"  t)
+        (""     "grffile"   t)
+        (""     "longtable" nil)
+        (""     "wrapfig"   nil)
+        (""     "rotating"  nil)
+        ("normalem" "ulem"  t)
+        (""     "amsmath"   t)
+        (""     "textcomp"  t)
+        (""     "amssymb"   t)
+        (""     "capt-of"   nil)
+        (""     "hyperref"  nil)
+        ("margin=2cm,nohead, nofoot"     "geometry")))
+
+(setq org-latex-default-class "jsarticle")

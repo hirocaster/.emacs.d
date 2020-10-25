0634720e1d8b492ffee5b7cfbfe42a6a927298e8
hirocaster <hohtsuka@gmail.com>
2020-10-25 22:03:32 +0900

    Remove custom-set-variables

--- init.el
+++ init.el
@@ -114,22 +114,7 @@
 (el-get-bundle web-mode)
 (el-get-bundle yaml-mode)
 
-
-
 (el-get-bundle init-loader)
 (init-loader-load "~/.emacs.d/inits")
 (init-loader-load "~/.emacs.d/secrets")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (wgrep-ag w3m string-inflection robe ripgrep ox-gfm org-seek flycheck-elixir flycheck-credo ac-ispell ac-alchemist bind-key))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+

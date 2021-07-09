9cc8e49c9d461d738d15e7bc85053fd46395ee03
hirocaster <hohtsuka@gmail.com>
2021-07-09 23:49:53 +0900

    Fix org-ref

--- inits/52-org-ref.el
+++ inits/52-org-ref.el
@@ -1,3 +1,5 @@
 (add-hook 'org-ref-clean-bibtex-entry-hook 'org-ref-replace-nonascii)
+(add-hook 'org-export-before-parsing-hook 'orcp-citeproc)
 (setq bibtex-dialect 'biblatex)
 
+(require 'org-ref)

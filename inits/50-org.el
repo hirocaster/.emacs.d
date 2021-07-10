d097e3ff118b27f258f60966bc16e452c11c1ac7
hirocaster <hohtsuka@gmail.com>
2021-07-10 10:16:10 +0900

    Setup textlint

--- inits/50-org.el
+++ inits/50-org.el
@@ -42,3 +42,15 @@
 (setq org-emphasis-regexp-components
       '("   ('\"{\x200B" "-     .,:!?;'\")}\\[\x200B" "
 ,\"'" "." 1))
+
+(flycheck-define-checker textlint
+  "A linter for prose."
+  :command ("textlint" "--format" "unix" source-inplace)
+  :error-patterns
+  ((warning line-start (file-name) ":" line ":" column ": "
+     (id (one-or-more (not (any " "))))
+     (message (one-or-more not-newline)
+       (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+     line-end))
+  :modes (text-mode markdown-mode gfm-mode org-mode))
+(add-to-list 'flycheck-checkers 'textlint)

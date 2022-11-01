1d910d4e1de1db6255bd0808e72a883ac8644790
hirocaster <hohtsuka@gmail.com>
2022-11-02 08:29:45 +0900

    Overwrite key bind yank-pop

--- inits/90-bind-key.el
+++ inits/90-bind-key.el
@@ -6,5 +6,6 @@
 (bind-key "C-x C-b" 'consult-buffer)
 (bind-key "C-x C-r" 'consult-recent-file)
 (bind-key "C-s" 'consult-line)
+(bind-key "M-y" 'consult-yank-pop)
 
 (bind-key "C-=" 'er/expand-region)

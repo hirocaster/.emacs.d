e9068a04a96b4d1ba8345bbe95d47c47bd588de1
hirocaster <hohtsuka@gmail.com>
2022-11-01 17:41:45 +0900

    Config show

---
+++ inits/00-show.el
@@ -0,0 +1,9 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; time
+(setq frame-title-format "%f")
+(size-indication-mode t)
+(setq display-time-24hr-format t)
+(display-time-mode t)

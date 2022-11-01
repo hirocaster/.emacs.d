2263f1dde30c98c98ba3ee4961fc6389e1d65528
hirocaster <hohtsuka@gmail.com>
2022-11-01 23:19:32 +0900

    Disable auto-save-list-file

--- inits/10-files.el
+++ inits/10-files.el
@@ -1,6 +1,8 @@
 (setq backup-directory-alist
       `(("." . ,(concat user-emacs-directory "backups"))))
 
+(setq auto-save-list-file-prefix nil)
+
 (defun files-setup ()
   (auto-save-visited-mode))
 

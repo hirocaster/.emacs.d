161c0a3fa6f0c30b7ae1455733e248abcbfa6a48
hirocaster <hohtsuka@gmail.com>
2022-11-01 17:35:11 +0900

    Setup el-get

--- init.el
+++ init.el
@@ -1,2 +1,16 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
+
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path (locate-user-emacs-file "el-get-user/recipes"))
+
+(el-get-bundle tarao/el-get-lock)
+(el-get-lock)

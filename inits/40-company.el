7aa4d4586c91e31b33ca5899b15ce5c16f2980ad
hirocaster <hohtsuka@gmail.com>
2022-11-02 09:12:40 +0900

    Key bind company-mode

--- inits/40-company.el
+++ inits/40-company.el
@@ -1,4 +1,10 @@
-(setq company-minimum-prefix-length 2)
-(setq company-selection-wrap-around t)
+(with-eval-after-load 'company
+  (setq company-minimum-prefix-length 2)
+  (setq company-selection-wrap-around t)
+
+  (global-set-key (kbd "C-M-i") 'company-complete)
+  (define-key company-active-map (kbd "C-h") nil)
+  (define-key company-active-map (kbd "C-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "<tab>") 'company-complete-selection))
 
 (add-hook 'emacs-startup-hook 'global-company-mode)

3ebac6e95a2188404a330eaaf12c20cff2d363cb
hirocaster <hohtsuka@gmail.com>
2022-11-16 17:00:49 +0900

    Disable save popup

--- inits/10-files.el
+++ inits/10-files.el
@@ -2,8 +2,3 @@
       `(("." . ,(concat user-emacs-directory "backups"))))
 
 (setq auto-save-list-file-prefix nil)
-
-(defun files-setup ()
-  (auto-save-visited-mode))
-
-(add-hook 'emacs-startup-hook #'files-setup)
